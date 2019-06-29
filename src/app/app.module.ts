6aa1352c9feb7020be04642f80efe26c60b43099
guitarooman14 <guitarooman14@gmail.com>
2019-06-29 11:26:46 +0200

    set up the router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './core/navbar/navbar.component';
 import {MapViewerModule} from './map-viewer/map-viewer.module';
 import {RouterStateSerializer, StoreRouterConnectingModule} from '@ngrx/router-store';
 import {CustomSerializer} from './shared/router-serializer';
 import {MetaReducer, StoreModule} from '@ngrx/store';
 import {reducers, State} from './app.reducer';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {storeFreeze} from 'ngrx-store-freeze';
 import {environment} from '../environments/environment';
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [storeFreeze]: [];
+export const metaReducers: MetaReducer<State>[] = environment.metaReducers;
 
 @NgModule({
   declarations: [
@@ -22,15 +21,16 @@ export const metaReducers: MetaReducer<State>[] = !environment.production ? [sto
   ],
   imports: [
     BrowserModule,
-    StoreRouterConnectingModule,
-    StoreModule.forRoot(reducers, {metaReducers: [storeFreeze]}),
+    StoreRouterConnectingModule.forRoot(),
+    StoreModule.forRoot(reducers, {metaReducers}),
     StoreDevtoolsModule.instrument({
       maxAge: 25
     }),
     MapViewerModule,
     AppRoutingModule,
   ],
-  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
+  providers: [{provide: RouterStateSerializer, useClass: CustomSerializer}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
